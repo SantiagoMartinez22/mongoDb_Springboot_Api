5f5afb309a274e037a76723fdd7be2c1c6c40825
SantiagoMartinez22 <smyardev2@gmail.com>
2025-04-28 17:48:24 -0500

    initial config

---
+++ src/main/java/com/apiMongo/tadb4/Tadb4Application.java
@@ -0,0 +1,13 @@
+package com.apiMongo.tadb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tadb4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tadb4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiMongo/tadb4/Tadb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiMongo.tadb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tadb4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
